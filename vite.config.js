3744e619f30e10d435df78acbf6acee4df427ad2
wilsontt <wilsontt@icloud.com>
2026-03-29 21:03:04 +0800

    修改vite設定

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   // ? 的後面是  正式環境，就是你儲存庫的名字 。
   // : 的後面是  開發中環境。
   // process.env.NODE_ENV 是 Vite 提供的環境變數，用於判斷當前是生產環境還是開發環境。
-  base: process.env.NODE_ENV === 'production' ? '/react-week7/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/react-week8/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0', // 監聽所有網路介面
